1f6a36e53bc175ad0b0914c89e059b6f68afeb0a
atul080 <atulsrivastav16k26@gmaial.com>
2021-12-23 09:44:52 +0530

    Added: Added attendance function.

--- EmpWages/src/com/empwage/EmployeeWageBuilder.java
+++ EmpWages/src/com/empwage/EmployeeWageBuilder.java
@@ -1,9 +1,18 @@
 package com.empwage;
 
+import java.util.*;
+
 public class EmployeeWageBuilder {
     public static void main(String[] ar)
     {
         System.out.println("Welcome to employee wage builder.");
+        int IS_PRESENT=1;
+        Random ran=new Random();
+        int empCheck=ran.nextInt(2);
+        if(empCheck==IS_PRESENT)
+            System.out.println("Employee is Present.");
+        else
+            System.out.println("Employee is Absent.");
     }
 
 }
