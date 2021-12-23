d463a14a01a3018227ec63f34a723830218379b1
atul080 <atulsrivastav16k26@gmaial.com>
2021-12-23 10:03:55 +0530

    Added: Added emp wage calculation functionality.

--- EmpWages/src/com/empwage/EmployeeWageBuilder.java
+++ EmpWages/src/com/empwage/EmployeeWageBuilder.java
@@ -6,13 +6,22 @@ public class EmployeeWageBuilder {
     public static void main(String[] ar)
     {
         System.out.println("Welcome to employee wage builder.");
-        int IS_PRESENT=1;
+        int IS_FULL_TIME=1;
+        int EMP_RATE_PER_HOUR=20;
+        int empHours=0;
+        int empWage=0;
         Random ran=new Random();
         int empCheck=ran.nextInt(2);
-        if(empCheck==IS_PRESENT)
+        if(empCheck==IS_FULL_TIME) {
+            empHours=8;
             System.out.println("Employee is Present.");
-        else
+        }
+        else {
+            empHours=0;
             System.out.println("Employee is Absent.");
+        }
+        empWage=empHours*EMP_RATE_PER_HOUR;
+        System.out.println("The daily wage of employee is: "+empWage);
     }
 
 }
