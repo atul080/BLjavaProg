f3c48442516b4ee9e99615b248cf38be55c3ea12
atul080 <atulsrivastav16k26@gmaial.com>
2021-12-23 09:20:59 +0530

    Added: Welcome file added.

---
+++ EmpWages/src/com/empwage/EmployeeWageBuilder.java
@@ -0,0 +1,9 @@
+package com.empwage;
+
+public class EmployeeWageBuilder {
+    public static void main(String[] ar)
+    {
+        System.out.println("Welcome to employee wage builder.");
+    }
+
+}
