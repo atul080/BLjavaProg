8f7a2bdfcda8f34819591ed5aa0c06399f099175
atul080 <atulsrivastav16k26@gmaial.com>
2021-12-30 14:17:02 +0530

    Added: switch case functionality for emp wages calculation

--- EmpWages/src/com/empwage/EmployeeWageBuilder.java
+++ EmpWages/src/com/empwage/EmployeeWageBuilder.java
@@ -3,31 +3,27 @@ package com.empwage;
 import java.util.*;
 
 public class EmployeeWageBuilder {
-    public static void main(String[] ar)
-    {
+    static int IS_FULL_TIME=1;
+    static int IS_PART_TIME=2;
+    static int EMP_RATE_PER_HOUR=20;
+    public static void main(String[] ar) {
         System.out.println("Welcome to employee wage builder.");
-        int IS_FULL_TIME=1;
-        int IS_PART_TIME=2;
-        int EMP_RATE_PER_HOUR=20;
-        int empHours=0;
-        int empWage=0;
-        Random ran=new Random();
-        int empCheck=ran.nextInt(3);
-        if(empCheck==IS_FULL_TIME) {
-            empHours=8;
-            System.out.println("Employee is Present.");
-        }
-        else if(empCheck==IS_PART_TIME)
-        {
-            empHours=4;
-            System.out.println("Employee is Absent.");
-        }
-        else {
-            empHours=0;
-            System.out.println("Employee is Absent.");
-        }
-        empWage=empHours*EMP_RATE_PER_HOUR;
-        System.out.println("The daily wage of employee is: "+empWage);
+        int empHours = 0;
+        int empWage = 0;
+        Random ran = new Random();
+        int empCheck = ran.nextInt(3);
+        switch (empCheck) {
+            case 1:
+                    empHours = 8;
+                    break;
+            case 2:
+                    empHours = 4;
+                    break;
+            default:
+                    empHours = 0;
+                    break;
+           }
+            empWage = empHours * EMP_RATE_PER_HOUR;
+            System.out.println("The daily wage of employee is: " + empWage);
     }
-
 }
