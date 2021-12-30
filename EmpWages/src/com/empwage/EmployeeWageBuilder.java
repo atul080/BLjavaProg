c8875385caf1f7f08680cd041b0f2cb47174bf80
atul080 <atulsrivastav16k26@gmaial.com>
2021-12-30 15:38:55 +0530

    Added:Emp wage calculation using class object functionality added.

--- EmpWages/src/com/empwage/EmployeeWageBuilder.java
+++ EmpWages/src/com/empwage/EmployeeWageBuilder.java
@@ -1,14 +1,16 @@
 package com.empwage;
 import java.util.Random;
 
-public class EmployeeWageBuilder {
+public class EmpWagesBuilder {
     public static final int IS_FULL_TIME=1;
     public static final int EMP_RATE_PER_HOUR=20;
     public static final int IS_PART_TIME=2;
     public static final int NO_OF_WORKING_DAYS=20;
     public static final int TOTAL_WORKING_HOURS=100;
-    public static void main(String args[]) {
-        System.out.println("Welcome to Employee Wage Builder");
+
+    public void calculateEmpWage()
+    {
+
         int empHours=0;
         int empWage=0;
         int totalEmployeeWage=0;
@@ -35,5 +37,10 @@ public class EmployeeWageBuilder {
             totalEmployeeWage=totalEmployeeWage+empWage;
         }
         System.out.println("The total employee wage of an employee for "+totalEmpHours+" hours or "+totalWorkingDays+" days is: "+ totalEmployeeWage);
+    }
+    public static void main(String args[]) {
+        System.out.println("Welcome to Employee Wage Builder");
+        EmpWagesBuilder emp= new EmpWagesBuilder();
+        emp.calculateEmpWage();
     }
 }
