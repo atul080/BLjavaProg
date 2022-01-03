c4fb48b61c91b0531dd754dfb516b2367c2d25e7
Atul Srivastav <atul.srivastav.ap@nielsen.com>
2022-01-03 20:54:15 +0530

    Added: Employee wage calculation for coampies functionality added.

--- EmpWages/src/com/empwage/EmployeeWageBuilder.java
+++ EmpWages/src/com/empwage/EmployeeWageBuilder.java
@@ -1,46 +1,47 @@
+/*
+ * @author Atul Srivastav
+ * @date 01/01/2022
+ * Employee wages calculation for companies.
+ * */
 package com.empwage;
 import java.util.Random;
 
-public class EmpWagesBuilder {
+public class EmployeWageBuilder {
     public static final int IS_FULL_TIME=1;
-    public static final int EMP_RATE_PER_HOUR=20;
     public static final int IS_PART_TIME=2;
-    public static final int NO_OF_WORKING_DAYS=20;
-    public static final int TOTAL_WORKING_HOURS=100;
 
-    public void calculateEmpWage()
-    {
 
+    public static int computeEmpWage(String company,int empRatePerHour, int noOfWorkingDays,int maxHoursPerMonth){
         int empHours=0;
         int empWage=0;
-        int totalEmployeeWage=0;
+        int totalEmployeeEage=0;
         int totalEmpHours=0;
         int totalWorkingDays=0;
-        int count=0;
-        while(totalEmpHours<TOTAL_WORKING_HOURS && totalWorkingDays<NO_OF_WORKING_DAYS){
+        while(totalEmpHours<maxHoursPerMonth && totalWorkingDays<noOfWorkingDays) {
             totalWorkingDays++;
-            Random random=new Random();
-            int empCheck= random.nextInt( 3);
-            switch(empCheck){
+            Random random = new Random();
+            int empCheck = random.nextInt(3);
+            switch (empCheck) {
                 case IS_FULL_TIME:
-                    empHours=8;
+                    empHours = 8;
                     break;
                 case IS_PART_TIME:
-                    empHours=4;
+                    empHours = 4;
                     break;
                 default:
-                    empHours=0;
+                    empHours = 0;
                     break;
             }
-            totalEmpHours=totalEmpHours+empHours;
-            empWage=empHours*EMP_RATE_PER_HOUR;
-            totalEmployeeWage=totalEmployeeWage+empWage;
+            empWage = empHours * empRatePerHour;
+            totalEmpHours = totalEmpHours + empHours;
+            System.out.println("Day "+ totalWorkingDays+" Employee Worked: "+empHours+" Employee Wage "+empWage);
         }
-        System.out.println("The total employee wage of an employee for "+totalEmpHours+" hours or "+totalWorkingDays+" days is: "+ totalEmployeeWage);
+        totalEmployeeEage=totalEmpHours*empRatePerHour;
+        System.out.println("The total employee wage is in "+ company+ " company is "+totalEmployeeEage+"\n");
+        return totalEmployeeEage;
     }
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Builder");
-        EmpWagesBuilder emp= new EmpWagesBuilder();
-        emp.calculateEmpWage();
+        computeEmpWage("TCS",15,2,10);
     }
 }
